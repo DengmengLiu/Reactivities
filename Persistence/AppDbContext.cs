c0312d8496d0b6a556fafb39d32444737e2b1042
DengmengLiu <Dengmeng.liu@student.uts.edu.au>
2025-03-15 17:11:23 +1100

    Section-2 finished

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions opts) : DbContext(opts)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
