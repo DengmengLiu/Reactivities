5081ffe4bf8db20006f472376b3cc3c327e62773
DengmengLiu <liudengmeng@outlook.com>
2025-04-17 15:10:22 +1000

    Implement GetActivityList query and refactor ActivitiesController to use MediatR

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,10 @@
+using Application.Activities.Queries;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => 
 {
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList>());
 
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
@@ -28,7 +29,6 @@ try
     await context.Database.MigrateAsync();
     await DbInitializer.SeedData(context);
 }
-
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -5,14 +5,14 @@ using Persistence;
 
 namespace Application.Activities.Queries;
 
-public class GetActivitiesList
+public class GetActivityList
 {
     public class Query : IRequest<List<Activity>> { }
     public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>>
     {
         public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
-            return await context.Activities.ToListAsync();
+            return await context.Activities.ToListAsync(cancellationToken);
         }
 
     }
