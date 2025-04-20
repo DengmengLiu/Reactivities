200ad300e8760a1d1ec58cc8c4755664e0a89601
DengmengLiu <Dengmeng.liu@student.uts.edu.au>
2025-04-20 22:31:06 +1000

    Refactor application structure: move App and Navbar components, implement ActivityDashboard and ActivityList components, and update styles

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,29 @@
+import { Box, Container, CssBaseline } from "@mui/material"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Navbar from "./Navbar"
+import ActivityDashboard from "../../features/activities/dashborad/ActivityDashboard"
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([])
+
+  useEffect(() => {
+    axios.get<Activity[]>("https://localhost:5001/api/activities")
+      .then(response => setActivities(response.data))
+
+    return () => { }
+  }, [])
+  return (
+    <Box sx={{ backgroundColor: '#eeeeee'}}>
+      <CssBaseline />
+      <Navbar />
+      <Container maxWidth='xl' sx={{ mt: 2 }}>
+       <ActivityDashboard activities={activities}/>
+      </Container>
+
+    </Box>
+  )
+
+}
+
+export default App
